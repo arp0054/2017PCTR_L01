125fcdd82ae9bcb5a8837031c2974de8c23a7ce2
smi0010 <smi0010@alu.ubu.es>
2017-03-06 18:01:25 +0100

    Eliminacion error hilos

--- src/p012/Hilos.java
+++ src/p012/Hilos.java
@@ -1,28 +1,30 @@
 package p012;
 
-public class Hilos implements Runnable{
+public class Hilos implements Runnable {
 
 	private Ball ball;
 	private Board board;
-	
-	public Hilos (Ball ball, Board board){
-		this.ball=ball;
-		this.board=board;
+
+	public Hilos(Ball ball, Board board) {
+		this.ball = ball;
+		this.board = board;
 	}
-	
+
 	@Override
 	public void run() {
-		while (true){
-			ball.reflect();
-			ball.move();
-			board.repaint();
-			try {
+		try {
+			while (true) {
+				ball.reflect();
+				ball.move();
+				board.repaint();
+
 				Thread.sleep(20);
-			} catch (InterruptedException e) {
-				//e.printStackTrace();
+
 			}
+		} catch (InterruptedException e) {
+			// e.printStackTrace();
 		}
-			
+
 	}
 
 }
